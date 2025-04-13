3b81ea69d720edb4e5067d83137d737c569abddc
the name <email@gmail.com>
2025-04-13 23:04:56 +0700

    connect database

---
+++ config/db/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://capcapwdaproject:NxUEJpOlzrfAtXTI@wdacapcap.qghke2b.mongodb.net/',
+        );
+        console.log('Connected Successfully');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { connect };
---
+++ src/index.js
@@ -0,0 +1,8 @@
+try{
+const db = require('../config/db/')
+
+db.connect();
+}
+catch(error){
+    console.log(error);
+}
