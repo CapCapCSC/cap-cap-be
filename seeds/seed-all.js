a02f51504be9dfb4eab0b02b5a0301a0350c176d
DiMinch <phanduyminh1262@gmail.com>
2025-04-20 19:04:01 +0700

    feat: add seed data for users, restaurants, vouchers, badges, questions, and quizzes; update food collection name

--- seeds/seed-all.js
+++ seeds/seed-all.js
@@ -12,8 +12,32 @@ const collections = [
         documents: require(path.join(__dirname, './seed-data/FoodTag.json')),
     },
     {
-        name: 'food',
+        name: 'foods',
         documents: require(path.join(__dirname, './seed-data/Food.json')),
+    },
+    {
+        name: 'users',
+        documents: require(path.join(__dirname, './seed-data/User.json')),
+    },
+    {
+        name: 'restaurants',
+        documents: require(path.join(__dirname, './seed-data/Restaurant.json')),
+    },
+    {
+        name: 'vouchers',
+        documents: require(path.join(__dirname, './seed-data/Voucher.json')),
+    },
+    {
+        name: 'badges',
+        documents: require(path.join(__dirname, './seed-data/Badge.json')),
+    },
+    {
+        name: 'questions',
+        documents: require(path.join(__dirname, './seed-data/Question.json')),
+    },
+    {
+        name: 'quizzes',
+        documents: require(path.join(__dirname, './seed-data/Quiz.json')),
     },
 ];
 
