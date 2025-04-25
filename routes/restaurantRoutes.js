41f6bb29d60ae9993955169c60e6acc6fcdca660
DiMinch <phanduyminh1262@gmail.com>
2025-04-25 09:29:39 +0700

    chore: update .env.example for improved configuration clarity, add Redis and email settings, and include cache middleware in restaurant routes

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -6,24 +6,27 @@ const adminMiddleware = require('../middlewares/adminMiddleware');
 const requestLogger = require('../middlewares/requestLogger');
 const validator = require('../middlewares/validationSchemas');
 const validate = require('../middlewares/validate');
+const { cache, clearCache } = require('../middlewares/cache');
 
+const CACHE_DURATION = 3600; // 1 hour
 // Apply request logger middleware
 router.use(requestLogger);
 
 //PUBLIC
-router.get('/', restaurantController.getAllRestaurants); // GET /api/restaurants
-router.get('/:id', restaurantController.getRestaurantById); // GET /api/restaurant/:id
-router.get('/random', restaurantController.getRandom3Restaurants); // GET /api/restaurant/random
+router.get('/', cache(CACHE_DURATION), restaurantController.getAllRestaurants); // GET /api/restaurants
+router.get('/:id', cache(CACHE_DURATION), restaurantController.getRestaurantById); // GET /api/restaurant/:id
+router.get('/random', cache(CACHE_DURATION), restaurantController.getRandom3Restaurants); // GET /api/restaurant/random
 
 //ADMIN
 router.post(
     '/',
+    clearCache,
     authMiddleware,
     adminMiddleware,
     validate(validator.createRestaurantSchema),
     restaurantController.createRestaurant,
 ); // POST /api/restaurant
-router.put('/:id', authMiddleware, adminMiddleware, restaurantController.updateRestaurant); // PUT /api/restaurant/:id
-router.delete('/:id', authMiddleware, adminMiddleware, restaurantController.deleteRestaurant); // DELETE /api/restaurant/:id
+router.put('/:id', clearCache, authMiddleware, adminMiddleware, restaurantController.updateRestaurant); // PUT /api/restaurant/:id
+router.delete('/:id', clearCache, authMiddleware, adminMiddleware, restaurantController.deleteRestaurant); // DELETE /api/restaurant/:id
 
 module.exports = router;
